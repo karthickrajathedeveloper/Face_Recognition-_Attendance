d1686686a8ca2ee91fb0e07a5a5ac31d482bb812
Karthickraja <130274513+karthickrajathedeveloper@users.noreply.github.com>
2025-02-17 11:40:43 +0530

    Face_attendance-Database

---
+++ face_attendance.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS face_attendance;
+USE face_attendance;
+show databases
+-- Table for storing registered users
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    face_encoding LONGBLOB NOT NULL
+);
+
+-- Table for storing attendance records
+CREATE TABLE IF NOT EXISTS attendance (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    date DATE,
+    time TIME,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
